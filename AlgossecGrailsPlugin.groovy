942436de3e66d7d94fe70ded8d64461769714a69
GuidoCeresa <guidoceresa@me.com>
2013-08-03 08:51:14 +0200

    0.25

--- AlgossecGrailsPlugin.groovy
+++ AlgossecGrailsPlugin.groovy
@@ -3,48 +3,32 @@
 //
 class AlgossecGrailsPlugin {
     // the plugin version
-    def version = "0.11"
+    def version = "0.25"
+
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2.3 > *"
+
     // the other plugins this plugin depends on
     def dependsOn = [:]
+    // mavenPublisher: "0.8 > *",     // maven-publisher
+
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/conf/PluginUrlMappings.groovy",
             "grails-app/conf/ApplicationResources.groovy",
             "grails-app/views/error.gsp",
-            "grails-app/controllers/algos/ProvaController.groovy",
-            "grails-app/domain/algos/Prova.groovy",
+            "grails-app/controllers/it/algos/algossec/ProvaController.groovy",
+            "grails-app/domain/it/algos/algossec/Prova.groovy",
             "grails-app/views/prova/"
     ]
 
-    // TODO Fill in these fields
-    def title = "Algossec Plugin" // Headline display name of the plugin
-    def author = "Gac"
-    def authorEmail = ""
-    def description = '''\
-Brief summary/description of the plugin.
-'''
+    def author = "Gac & Alex"
+    def authorEmail = "gac@algos.it"
+    def title = "Libreria di base per Grails"
+    def description = 'Libreria di funzionalita comuni a qualsiasi applicazione realizzata con Grails'
 
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/algossec"
-
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
+    def documentation = "http://grails.org/plugin/algos"
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
